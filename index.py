676d206124834c15894ff60d354650a149553079
Adham Farrag <adham.farrag@hotmail.com>
2022-12-14 18:38:04 +0800

    👌 IMPROVE: other servo

--- index.py
+++ index.py
@@ -4,9 +4,9 @@ from time import sleep
 servo = AngularServo(18, min_pulse_width=0.0006, max_pulse_width=0.0023)
 
 while (True):
-    servo.angle = 90
+    servo.angle = 45
     sleep(2)
-    servo.angle = 0
+    servo.angle = 10
     sleep(2)
-    servo.angle = -90
+    servo.angle = 45
     sleep(2)
