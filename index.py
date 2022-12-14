3d5f5c9f34a445d53473f86fb79a17a227fc451c
Adham Farrag <adham.farrag@hotmail.com>
2022-12-15 00:36:11 +0800

    👌 IMPROVE: 10 seconds

--- index.py
+++ index.py
@@ -19,7 +19,6 @@ GPIO.setup(Motor2A,GPIO.OUT)
 GPIO.setup(Motor2B,GPIO.OUT)
 GPIO.setup(Motor2E,GPIO.OUT)
 
-print ("Going forwards")
 GPIO.output(Motor1A,GPIO.HIGH)
 GPIO.output(Motor1B,GPIO.LOW)
 GPIO.output(Motor1E,GPIO.HIGH)
@@ -28,21 +27,4 @@ GPIO.output(Motor2A,GPIO.HIGH)
 GPIO.output(Motor2B,GPIO.LOW)
 GPIO.output(Motor2E,GPIO.HIGH)
 
-sleep(2)
-
-print ("Going backwards")
-GPIO.output(Motor1A,GPIO.LOW)
-GPIO.output(Motor1B,GPIO.HIGH)
-GPIO.output(Motor1E,GPIO.HIGH)
-
-GPIO.output(Motor2A,GPIO.LOW)
-GPIO.output(Motor2B,GPIO.HIGH)
-GPIO.output(Motor2E,GPIO.HIGH)
-
-sleep(2)
-
-print ("Now stop")
-GPIO.output(Motor1E,GPIO.LOW)
-GPIO.output(Motor2E,GPIO.LOW)
-
-GPIO.cleanup()
+sleep(10)
