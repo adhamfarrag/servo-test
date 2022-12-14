ba3c106951b72b5f6d162fefb99ed0417cf8f541
Adham Farrag <adham.farrag@hotmail.com>
2022-12-14 20:29:21 +0800

    👌 IMPROVE: values

--- index.py
+++ index.py
@@ -5,10 +5,10 @@ servo = AngularServo(18, min_pulse_width=0.0006, max_pulse_width=0.0023)
 
 while (True):
     # test values for a gripper servo
-    servo.angle = 180
-    sleep(4)
-    servo.angle = 90
+    servo.angle = -90
     sleep(4)
     servo.angle = 0
+    sleep(4)
+    servo.angle = 90
     sleep(4)
 
