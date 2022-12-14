ddba7a229494995683bc2e3e747082f11c692b9c
Adham Farrag <adham.farrag@hotmail.com>
2022-12-15 07:22:56 +0800

    👌 IMPROVE: pin number

--- test_servo.py
+++ test_servo.py
@@ -1,7 +1,7 @@
 from gpiozero import AngularServo
 from time import sleep
 
-servo = AngularServo(18, min_pulse_width=0.0006, max_pulse_width=0.0023)
+servo = AngularServo(36, min_pulse_width=0.0006, max_pulse_width=0.0023)
 
 while (True):
     servo.angle = 90
