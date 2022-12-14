e78ad174caa01f13f205fe9242861c765ce6a65f
Adham Farrag <adham.farrag@hotmail.com>
2022-12-14 20:37:25 +0800

    👌 IMPROVE: Dual

--- index.py
+++ index.py
@@ -1,14 +1,19 @@
 from gpiozero import AngularServo
 from time import sleep
 
-servo = AngularServo(18, min_pulse_width=0.0006, max_pulse_width=0.0023)
+servoOne = AngularServo(23, min_pulse_width=0.0006, max_pulse_width=0.0023)
+servoTwo = AngularServo(24, min_pulse_width=0.0006, max_pulse_width=0.0023)
+
 
 while (True):
     # test values for a gripper servo
-    servo.angle = -90
+    servoOne.angle = -90
+    servoTwo.angle = -90
     sleep(4)
-    servo.angle = 0
+    servoOne.angle = 0
+    servoTwo.angle = 0
     sleep(4)
-    servo.angle = 90
+    servoOne.angle = 90
+    servoTwo.angle = 90
     sleep(4)
 
