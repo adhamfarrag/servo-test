585e02999bfa277b7ab4590d29f3ccdc1a5245a3
Adham Farrag <adham.farrag@hotmail.com>
2022-12-15 07:24:18 +0800

    👌 IMPROVE: indicators

--- test_servo.py
+++ test_servo.py
@@ -5,8 +5,13 @@ servo = AngularServo(36, min_pulse_width=0.0006, max_pulse_width=0.0023)
 
 while (True):
     servo.angle = 90
+    print("Servo angle: ", servo.angle)
     sleep(2)
+
     servo.angle = 0
+    print("Servo angle: ", servo.angle)
     sleep(2)
+
     servo.angle = -90
+    print("Servo angle: ", servo.angle)
     sleep(2)
