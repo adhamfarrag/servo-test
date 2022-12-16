6fa89530a3994b08e6b521f37183de4aca289f73
Adham Farrag <adham.farrag@hotmail.com>
2022-12-16 18:48:44 +0800

    👌 IMPROVE: output

--- sensor.py
+++ sensor.py
@@ -11,7 +11,7 @@ def distance(measure='cm'):
     GPIO.setup(Echo, GPIO.IN)
 
     time.sleep(0.3)
-    GPIO.ouput(12, True)
+    GPIO.output(12, True)
     time.sleep(0.00001)
 
     GPIO.output(12, False)
